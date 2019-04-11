e210d71347db7cd9575e3ebe7a8788764a8b3e01
Ela Demir <ati@TAYFUN-MacBook-Pro.local>
2019-04-12 01:37:19 +0300

    angular todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'sunum';
+  todoArray = [];
+  todo;
+  addTodo(value) {
+    if (value !== "") {
+      this.todoArray.push(value);
+    }
+  }
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
 }
